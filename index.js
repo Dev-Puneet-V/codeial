3c9b6a2ce56dfb440db7edbc7a3f12a3a470d603
Puneet-iner <imhtcy312@gmail.com>
2020-06-12 11:41:23 +0530

    First commit: create the directory and setup express

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const port = 8000;
+
+
+//Start server
+app.listen(port, function(err){
+    if(err){
+        console.log(`Error in running the server : ${err}`);//this is called interpolation, writing variabe inside string
+        return;
+    }
+
+    console.log(`Server is running on port: ${port}`);
+});
