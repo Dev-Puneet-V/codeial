8833c5c9b01b5b27bc5b10fe718da8be2cc5f88e
Puneet-iner <imhtcy312@gmail.com>
2020-06-13 09:45:11 +0530

    add a controller and a router from home

---
+++ controllers/home_controller.js
@@ -0,0 +1,6 @@
+//in this controller is a set of different actions
+module.exports.home = function(req, res){
+    return res.end('<h1>Express is Up for coding</h1>');
+}
+
+//module.exports.actionName = function(req, res){}
---
+++ routes/index.js
@@ -0,0 +1,7 @@
+const express = require('express');
+
+const router = express.Router();
+const homeController = require('../controllers/home_controller')
+console.log("router loaded");
+router.get('/', homeController.home);
+module.exports = router;//here we can't write only exports, module is neccessary
