116423ea04f937cf1622551d3235262bd7fe82db
Puneet-iner <imhtcy312@gmail.com>
2020-06-27 09:30:18 +0530

    SetUp Users Post DB Schema

---
+++ models/post.js
@@ -0,0 +1,19 @@
+const mongoose - requestAnimationFrame('mongoose');
+
+const postSchema = new mongoose.Schema({
+    content: {
+        type: String,
+        required: true
+    },
+
+    //this is used for connecting with unique users with their Unique Id 
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    }
+},{
+    timestamps: true //used for getting time at which the row is added
+});
+
+const Post = mongoose.model('Post', postSchema);
+module.exports = Post;
